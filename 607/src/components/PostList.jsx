dfc132f41a8187a5dd916fcf35832ca2224c975a
Charles Ross <chivalry@mac.com>
2025-09-29 14:03:29 -0700

    changeBodyHandler

--- 607/src/components/PostList.jsx
+++ 607/src/components/PostList.jsx
@@ -1,12 +1,16 @@
 import Post from './Post';
+import NewPost from './NewPost';
 import classes from './PostList.module.css';
 
 function PostList() {
     return (
-        <ul className={classes.posts}>
-            <Post author="Maximillian" body="React is awesome!" />
-            <Post author="Manuel" body="React is great!" />
-        </ul>
+        <>
+            <NewPost />
+            <ul className={classes.posts}>
+                <Post author="Maximillian" body="React is awesome!" />
+                <Post author="Manuel" body="React is great!" />
+            </ul>
+        </>
     );
 }
 
