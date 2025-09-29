e96882487f869dbbb2622cf6c69e794c6a3f0090
Charles Ross <chivalry@mac.com>
2025-09-29 09:40:05 -0700

    variable name

--- 607/src/components/Post.jsx
+++ 607/src/components/Post.jsx
@@ -1,7 +1,10 @@
+const names = ['Maximillian', 'Manuel'];
+
 function Post() {
+    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
     return (
         <div>
-            <p>Maximillian</p>
+            <p>{chosenName}</p>
             <p>React is awesome!</p>
         </div>
     );
